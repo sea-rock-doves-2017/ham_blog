f0264617092891af04f848eba5acf5e38b5d080b
Jordan Kamin <jordanakamin@gmail.com>
2017-07-17 14:41:56 -0700

    builds/tests title presence validator

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 describe Article do
   it 'belongs to a user'
   context 'validations' do
-    it 'validates presence of title'
+    let(:invalid_article) {Article.new}
+    let(:article) {Article.new(title: 'ham', body: 'not as funny as it once was')}
+    it 'is valid with a title' do
+      expect(article).to be_valid
+    end
+    it 'is invalid without a title' do
+      expect(invalid_article).to_not be_valid
+    end
     it 'validates presence of body'
   end
 end
