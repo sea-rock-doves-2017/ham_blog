9f9caa06cd6d596e59ad3f5e926ed0c147505fa2
Jordan Kamin <jordanakamin@gmail.com>
2017-07-18 09:38:32 -0700

    adds status test for ArticlesController#index

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -3,15 +3,28 @@ require 'rails_helper'
 describe ArticlesController do
   let!(:article) {Article.create(title: 'test database!', body: 'A necessary and wonderful database')}
   describe '#index' do
-    it 'assigns the @articles variable' do
+    before(:each) do
       get :index
+    end
+    it 'assigns the @articles variable' do
       expect(assigns[:articles]).to eq(Article.all)
     end
+    it 'responds with a status of 200' do
+      expect(response.status).to eq(200)
+    end
   end
-  describe '#update' do
-    xit 'updates an article in th database' do
+  describe '#show' do
+  end
+  describe '#new' do
+  end
+  describe '#edit' do
+  end
+  xdescribe '#update' do
+    it 'updates an article in the database' do
       put :update, params: {id: article.id}
 
     end
+  end
+  describe '#delete' do
   end
 end
